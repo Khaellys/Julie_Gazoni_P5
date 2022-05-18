4caadc1217b708fc9ff91a54d1ddc107fd4ed1ae
Khaellys <julie.dellena@hotmail.fr>
2022-05-18 16:04:59 +0200

    page product.js ajout de la gestion du panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,5 @@
-var str = window.location.href; //URL de la page courante
-var url = new URL(str);
+var page = window.location.href; //URL de la page courante
+var url = new URL(page);
 var articleId = url.searchParams.get("id");
 console.log(articleId);
 
@@ -26,31 +26,46 @@ function callProduct(kanap){
 
  //Création élément Image       
 let articleImg = document.createElement('img')
-document.querySelector('.item__img').appendChild(articleImg)
-articleImg.src = kanap.imageUrl;
-articleImg.alt = kanap.altTxt;
+    document.querySelector('.item__img').appendChild(articleImg)
+    articleImg.src = kanap.imageUrl;
+    articleImg.alt = kanap.altTxt;
 
 // Insertion du Nom
 let articleName = document.getElementById('title')
-articleName.innerHTML = kanap.name;
+    articleName.innerHTML = kanap.name;
 
 // Insertion du prix
 let articlePrice = document.getElementById('price')
-articlePrice.innerHTML = kanap.price;
+    articlePrice.innerHTML = kanap.price;
 
 // Insertion de la Description
 let articleDescription = document.getElementById('description')
-articleDescription.innerHTML = kanap.description;
+    articleDescription.innerHTML = kanap.description;
 
 // Insertion des Couleurs
 for (let colors of kanap.colors){
     console.table(colors)
     let articleColor = document.createElement('option')
-    document.getElementById('colors').appendChild(articleColor);
-    articleColor.value = colors;
-    articleColor.innerHTML = colors;
+        document.getElementById('colors').appendChild(articleColor);
+        articleColor.value = colors;
+        articleColor.innerHTML = colors;
 }
 }
+
+// Panier
+function addToCart(kanap){
+    const addPanier = document.getElementById('addToCart');
+    addPanier.addEventListener('click', (event) => {
+// Conditions : quantité entre 1 et 100, et choix d'une couleur        
+        if(itemQuantity > 0 && itemQuantity <= 100 && articleColor > 0){
+        addPanier.innerHTML = "Article ajouté au panier";
+         } 
+    });
+}
+
+// Récupération dans le stockage local du panier
+localStorage.articleId = this.articleId;
+localStorage.articleColor = colors;
 
 //.catch(function(error) {
     //console.log('Oups !... Une erreur est survenue ');
